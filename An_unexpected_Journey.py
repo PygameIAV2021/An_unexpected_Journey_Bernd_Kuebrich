61324e27a32dbf05f0163061d1021bf9f5470025
Restless87 <Berndkuebrich@gmail.com>
2021-01-15 21:18:17 +0100

    Refactoring

--- An_unexpected_Journey.py
+++ An_unexpected_Journey.py
@@ -9,7 +9,6 @@ Created on Fri Oct 30 07:32:48 2020
 from map import *                       # Map importieren
 from classes import *                   # Klassen importieren
 
-#Inits
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -21,34 +20,10 @@ FPS = 15
 GAME_RUNNING = True
 
 #Spielerinstanz erzeugen
-classlink = Spielfiguren("Link", 50,50, "top")
-
-#Spielerfigur Sprites
-link_go_up = [pygame.image.load("sprites/link/link_b0.png"), pygame.image.load("sprites/link/link_b1.png"), pygame.image.load("sprites/link/link_b2.png"), pygame.image.load("sprites/link/link_b3.png"), pygame.image.load("sprites/link/link_b4.png"), pygame.image.load("sprites/link/link_b5.png"), pygame.image.load("sprites/link/link_b6.png"), pygame.image.load("sprites/link/link_b7.png")]
-link_go_down = [pygame.image.load("sprites/link/link_f0.png"), pygame.image.load("sprites/link/link_f1.png"), pygame.image.load("sprites/link/link_f2.png"), pygame.image.load("sprites/link/link_f3.png"), pygame.image.load("sprites/link/link_f4.png"), pygame.image.load("sprites/link/link_f5.png"), pygame.image.load("sprites/link/link_f6.png"), pygame.image.load("sprites/link/link_f4.png")]
-link_go_left = [pygame.image.load("sprites/link/link_l3.png"), pygame.image.load("sprites/link/link_l1.png"), pygame.image.load("sprites/link/link_l2.png"), pygame.image.load("sprites/link/link_l0.png"), pygame.image.load("sprites/link/link_l4.png"), pygame.image.load("sprites/link/link_l5.png"), pygame.image.load("sprites/link/link_l6.png"), pygame.image.load("sprites/link/link_l7.png")]
-link_go_right = [pygame.image.load("sprites/link/link_r0.png"), pygame.image.load("sprites/link/link_r1.png"), pygame.image.load("sprites/link/link_r2.png"), pygame.image.load("sprites/link/link_r3.png"), pygame.image.load("sprites/link/link_r4.png"), pygame.image.load("sprites/link/link_r5.png"), pygame.image.load("sprites/link/link_r6.png"), pygame.image.load("sprites/link/link_r7.png")]
+classlink = Spielfiguren("Link", 50,300, "down")
 
 #main loop
 while GAME_RUNNING:
-    # GAME MAP
-    for x in range(0, MAPHEIGHT):
-        for y in range(0, MAPWIDTH):
-            texture = spielfeld[x][y].type
-            DISPLAYSURFACE.blit(TEXTURES[texture], (y * TILESIZE, x * TILESIZE))
-
-    #Spielfigursprites und Spielfigurrechteck anzeigen
-    if classlink.look == "top":
-        DISPLAYSURFACE.blit(link_go_up[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-
-    elif classlink.look == "down":
-        DISPLAYSURFACE.blit(link_go_down[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-
-    elif classlink.look == "left":
-        DISPLAYSURFACE.blit(link_go_left[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-
-    elif classlink.look == "right":
-        DISPLAYSURFACE.blit(link_go_right[classlink.sprite], (classlink.rect.left, classlink.rect.top))
 
     #Überprüfen, ob Nutzer eine Aktion durchgeführt hat
     for event in pygame.event.get():
@@ -60,46 +35,26 @@ while GAME_RUNNING:
 
     #Bewegung nach Oben
     if buffer[pygame.K_UP]:
-        classlink.look = "top"
-        DISPLAYSURFACE.blit(link_go_up[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-        if classlink.rect.top >= classlink.speed: #if Bedingung damit die figur nicht über den oberen rand hinausgeht
-            classlink.tryToMove(0, -1*classlink.speed)
-            classlink.sprite += 1
-            if classlink.sprite > 7:
-                classlink.sprite = 0
-
+        move = classlink.tryToMove("up")
     #Bewegung nach Unten
     elif buffer[pygame.K_DOWN]:
-        classlink.look = "down"
-        DISPLAYSURFACE.blit(link_go_down[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-        if classlink.rect.top < (MAPHEIGHT * TILESIZE) -TILESIZE: #if Bedingung damit die figur nicht über den unteren rand hinausgeht
-            classlink.tryToMove(0, classlink.speed)
-            classlink.sprite += 1
-            if classlink.sprite > 7:
-                classlink.sprite = 0
-
+        classlink.tryToMove("down")
     #Bewegung nach Rechts
     elif buffer[pygame.K_RIGHT]:
-        classlink.look = "right"
-        DISPLAYSURFACE.blit(link_go_right[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-        if classlink.rect.left < (MAPWIDTH * TILESIZE) -TILESIZE: #if Bedingung damit die figur nicht über den rechten rand hinausgeht
-            classlink.tryToMove(classlink.speed, 0)
-            classlink.sprite += 1
-            if classlink.sprite > 7:
-                classlink.sprite = 0
-
+        classlink.tryToMove("right")
     #Bewegung nach Links
     elif buffer[pygame.K_LEFT]:
-        classlink.look = "left"
-        DISPLAYSURFACE.blit(link_go_left[classlink.sprite], (classlink.rect.left, classlink.rect.top))
-        if classlink.rect.left > 0:         #if Bedingung damit die figur nicht über den linken rand hinausgeht
-            classlink.tryToMove(-1 * classlink.speed, 0)
-            classlink.sprite += 1
-            if classlink.sprite > 7:
-                classlink.sprite = 0
+        classlink.tryToMove("left")
     else:
-        classlink.sprite = 0 #Damit das richtige Spielerbild beim Stehen angezeigt wird
+        classlink.spritecounter = 0 #Damit das richtige Spielerbild beim Stehen angezeigt wird
 
+    # GAME MAP
+    for x in range(0, MAPHEIGHT):
+        for y in range(0, MAPWIDTH):
+            texture = spielfeld[x][y].type
+            DISPLAYSURFACE.blit(TEXTURES[texture], (y * TILESIZE, x * TILESIZE))
+
+    classlink.draw()
     #Screen aktualisieren
     pygame.display.update()
     clock.tick(FPS)
--- classes.py
+++ classes.py
@@ -4,24 +4,41 @@ from map import *
 spielfeld = [[0 for y in range(MAPWIDTH)] for x in range(MAPHEIGHT)]
 
 class Spielfiguren:
-    def __init__(self, name, pos_x, pos_y, look, speed=10, sprite=0):
+    def __init__(self, name, pos_x, pos_y, look, speed=10, spritecounter=0):
         self.name = name
         self.speed = speed
         self.look = look
         self.rect = pygame.Rect(pos_x, pos_y, TILESIZE, TILESIZE)
-        self.sprite = sprite
+        self.spritecounter = spritecounter
+        self.link_go_up = [pygame.image.load("sprites/link/link_b0.png"), pygame.image.load("sprites/link/link_b1.png"), pygame.image.load("sprites/link/link_b2.png"), pygame.image.load("sprites/link/link_b3.png"), pygame.image.load("sprites/link/link_b4.png"), pygame.image.load("sprites/link/link_b5.png"), pygame.image.load("sprites/link/link_b6.png"), pygame.image.load("sprites/link/link_b7.png")]
+        self.link_go_down = [pygame.image.load("sprites/link/link_f0.png"), pygame.image.load("sprites/link/link_f1.png"), pygame.image.load("sprites/link/link_f2.png"), pygame.image.load("sprites/link/link_f3.png"), pygame.image.load("sprites/link/link_f4.png"), pygame.image.load("sprites/link/link_f5.png"), pygame.image.load("sprites/link/link_f6.png"), pygame.image.load("sprites/link/link_f4.png")]
+        self.link_go_left = [pygame.image.load("sprites/link/link_l3.png"), pygame.image.load("sprites/link/link_l1.png"), pygame.image.load("sprites/link/link_l2.png"), pygame.image.load("sprites/link/link_l0.png"), pygame.image.load("sprites/link/link_l4.png"), pygame.image.load("sprites/link/link_l5.png"), pygame.image.load("sprites/link/link_l6.png"), pygame.image.load("sprites/link/link_l7.png")]
+        self.link_go_right = [pygame.image.load("sprites/link/link_r0.png"), pygame.image.load("sprites/link/link_r1.png"), pygame.image.load("sprites/link/link_r2.png"), pygame.image.load("sprites/link/link_r3.png"), pygame.image.load("sprites/link/link_r4.png"), pygame.image.load("sprites/link/link_r5.png"), pygame.image.load("sprites/link/link_r6.png"), pygame.image.load("sprites/link/link_r7.png")]
 
-    def tryToMove(self, diffx, diffy):
-        # diffx = 0
-        # diffy = 0
-        #
-        # if direction == 'top:
-        #     diffx = self.speed
-        # elif direction == 'left':
-        #     diffy = -1* self.speed
-        #todo: anstatt diffX und diffy einfach 'top', 'down', left, right übergeben
-        #todo: überprüfe ob man über rand geht
-        #todo: link frame machen
+    def tryToMove(self, direction):
+
+        moveX, moveY = (0, 0)
+
+        if direction == "down":
+            self.look = "down"
+            #DISPLAYSURFACE.blit(self.link_go_down[self.spritecounter], (self.rect.left, self.rect.top))
+            if self.rect.top < ( MAPHEIGHT * TILESIZE) - TILESIZE:  # if Bedingung damit die figur nicht über den unteren rand hinausgeht
+                moveX, moveY = (0, self.speed)
+        elif direction == "up":
+            self.look = "up"
+            #DISPLAYSURFACE.blit(self.link_go_up[self.spritecounter], (self.rect.left, self.rect.top))
+            if self.rect.top >= self.speed:
+                moveX, moveY = (0, -1*self.speed)
+        elif direction == "right":
+            self.look = "right"
+            #DISPLAYSURFACE.blit(self.link_go_right[self.spritecounter], (self.rect.left, self.rect.top))
+            if self.rect.left < (MAPWIDTH * TILESIZE) - TILESIZE:  # if Bedingung damit die figur nicht über den rechten rand hinausgeht
+                moveX, moveY = (self.speed, 0)
+        elif direction ==  "left":
+            self.look = "left"
+            #DISPLAYSURFACE.blit(self.link_go_left[self.spritecounter], (self.rect.left, self.rect.top))
+            if self.rect.left > 0:  # if Bedingung damit die figur nicht über den linken rand hinausgeht
+                moveX, moveY = (-1*self.speed, 0)
 
         collisionList = [0 for x in range(MAPWIDTH*MAPHEIGHT)]
         i = 0
@@ -30,7 +47,7 @@ class Spielfiguren:
                 collisionList[i] = spielfeld[x][y]
                 i += 1
 
-        self.rect.move_ip(diffx, diffy) #verändere dich um diffx und diffy
+        self.rect.move_ip(moveX, moveY) #verändere deine Position um diffx und diffy
 
         result = self.rect.collidelistall(collisionList) #liefert index (aus collisionList) von allen rects mit kollisionen
 
@@ -38,8 +55,29 @@ class Spielfiguren:
         for index in result:
             print(collisionList[index].type)
             if collisionList[index].type in [Tiles.WATER, Tiles.WALL]:
-                self.rect.move_ip(-1*diffx, -1*diffy)
+                self.rect.move_ip(-1*moveX, -1*moveY)
                 return
+
+        self.spritecounter += 1
+        if self.spritecounter > 7:
+            self.spritecounter = 0
+            
+    def draw(self):
+        if self.look == "up":
+            DISPLAYSURFACE.blit(self.link_go_up[self.spritecounter],
+                                (self.rect.left, self.rect.top))
+
+        elif self.look == "down":
+            DISPLAYSURFACE.blit(self.link_go_down[self.spritecounter],
+                                (self.rect.left, self.rect.top))
+
+        elif self.look == "left":
+            DISPLAYSURFACE.blit(self.link_go_left[self.spritecounter],
+                                (self.rect.left, self.rect.top))
+
+        elif self.look == "right":
+            DISPLAYSURFACE.blit(self.link_go_right[self.spritecounter],
+                                (self.rect.left, self.rect.top))
 
 class Spielkachel(pygame.Rect):
     def __init__(self, typ, x, y):
